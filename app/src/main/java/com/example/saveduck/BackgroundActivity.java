95b0cd584af7965e2391dc2bdb3dbb3ee033757f
Javier M <javier_m_h_1993@hotmail.com>
2023-05-07 19:46:01 +0200

    Diseño mejorado de la gráfica

--- app/src/main/java/com/example/saveduck/BackgroundActivity.java
+++ app/src/main/java/com/example/saveduck/BackgroundActivity.java
@@ -14,7 +14,7 @@ import org.eazegraph.lib.models.PieModel;
 
 public class BackgroundActivity extends AppCompatActivity {
 
-    TextView tvR, tvPython, tvCPP, tvJava;
+    TextView tvR, tvPython, tvCPP;
     PieChart pieChart;
 
     @Override
@@ -29,7 +29,6 @@ public class BackgroundActivity extends AppCompatActivity {
         tvR = backBinding.tvR;
         tvPython = backBinding.tvPython;
         tvCPP = backBinding.tvCPP;
-        tvJava = backBinding.tvJava;
         pieChart = backBinding.piechart;
 
         setData();
@@ -45,31 +44,25 @@ public class BackgroundActivity extends AppCompatActivity {
     {
 
         // Set the percentage of language used
-        tvR.setText(Integer.toString(40));
-        tvPython.setText(Integer.toString(30));
-        tvCPP.setText(Integer.toString(5));
-        tvJava.setText(Integer.toString(25));
+        tvR.setText(Integer.toString(10));
+        tvPython.setText(Integer.toString(10));
+        tvCPP.setText(Integer.toString(10));
 
         // Set the data and color to the pie chart
         pieChart.addPieSlice(
                 new PieModel(
                         "R",
                         Integer.parseInt(tvR.getText().toString()),
-                        Color.parseColor("#FFA726")));
+                        Color.parseColor("#66BB6A")));
         pieChart.addPieSlice(
                 new PieModel(
                         "Python",
                         Integer.parseInt(tvPython.getText().toString()),
-                        Color.parseColor("#66BB6A")));
+                        Color.parseColor("#EF5350")));
         pieChart.addPieSlice(
                 new PieModel(
                         "C++",
                         Integer.parseInt(tvCPP.getText().toString()),
-                        Color.parseColor("#EF5350")));
-        pieChart.addPieSlice(
-                new PieModel(
-                        "Java",
-                        Integer.parseInt(tvJava.getText().toString()),
                         Color.parseColor("#29B6F6")));
 
         // To animate the pie chart
